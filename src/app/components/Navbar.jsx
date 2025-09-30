94b1bb2002847289f8688f8ddfbdca326fd41466
tiyawt <tiyaland3@gmail.com>
2025-09-30 22:02:02 +0700

    styling nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,8 +8,8 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-transparent absolute top-0 left-0 z-20">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
-        <Link href="/" className="text-xl font-bold text-neon">
-          G<span className="text-yellow-400">Dev</span>
+        <Link href="/" className="text-xl font-bold text-green-600">
+          Green<span className="text-yellow-300">Cycle</span>
         </Link>
 
         <ul className="hidden md:flex gap-8 text-xs uppercase tracking-wider font-bold">
@@ -17,7 +17,7 @@ export default function Navbar() {
             <Link
               href="/"
               className={`hover:text-neon ${
-                pathname === "/" ? "text-[#287351]" : "text-[#6D974C]"
+                pathname === "/" ? "text-yellow-300" : "text-green-600"
               }`}
             >
               Home
@@ -28,8 +28,8 @@ export default function Navbar() {
               href="/wastebank"
               className={`hover:text-neon ${
                 pathname.startsWith("/wastebank")
-                  ? "text-[#287351]"
-                  : "text-[#6D974C]"
+                  ? "text-yellow-300"
+                  : "text-green-600"
               }`}
             >
               Kirim sampah
@@ -39,7 +39,7 @@ export default function Navbar() {
             <Link
               href="/game"
               className={`hover:text-neon ${
-                pathname.startsWith("/game") ? "text-neon" : "text-[#6D974C]"
+                pathname.startsWith("/game") ? "text-yellow-300" : "text-green-600"
               }`}
             >
               Game
@@ -49,7 +49,7 @@ export default function Navbar() {
 
         <Link
           href="/transactions"
-          className="bg-neon text-[#6D974C] px-4 py-2 rounded shadow hover:opacity-90 font-bold"
+          className="bg-neon text-green-600 px-4 py-2 rounded shadow hover:text-yellow-300 font-bold"
         >
           Transaksi
         </Link>
