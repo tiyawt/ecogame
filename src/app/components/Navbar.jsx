8e135999c1e14f1eddadb9c0f74699433a3ab095
tiyawt <tiyaland3@gmail.com>
2025-10-03 11:51:14 +0700

    add bg

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
     `hover:text-neon ${active ? "text-yellow-300" : "text-green-600"}`;
 
   return (
-    <nav className="w-full bg-transparent absolute top-0 left-0 z-20">
+    <nav className="w-full bg-white absolute top-0 left-0 z-20">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4 ">
         <Link
           href="/"
